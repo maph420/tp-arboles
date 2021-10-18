c8fd76c4598e8439fe2cedb19e27b59f22b0a534
maph <71228707+maph420@users.noreply.github.com>
2021-10-18 17:44:14 -0300

    Update arbol.h

--- tp_arboles/arbol.h
+++ tp_arboles/arbol.h
@@ -30,17 +30,23 @@ int isEmpty(Arbol a);
 
 typedef int (*Comparar) ( void * dato1, int dato2 );
 
-typedef int(*CompararNodos) ( void* dato1, Judoca dato2);
+typedef int(*CompararNodos) ( void* dato1, Judoca dato2, Arbol* a1, Arbol* a2);
 
 
 Arbol agrega(Arbol a, char* judocaNombre, char* judocaApellido, int judocaEdad, Comparar c);
 
 void muestraPreOrder(Arbol a);
 
-void compararNodo(Arbol a, CompararNodos c, Judoca nodoJudoca);
+void compararNodo(Arbol a, CompararNodos c, Judoca nodoJudoca, Arbol* a2);
 
 void compararArboles(Arbol a1, Arbol a2, CompararNodos c);
 
 void testJudoca();
+
+typedef void (* Destruir) (void * dato);
+void arbol_destruir(Arbol arbolN, Destruir d);
+
+Arbol elimina(Arbol a, int dato);
+
 
 #endif /* __ARBOL_H__ */
